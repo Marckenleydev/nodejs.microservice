c3e452ebf0099f4e2c56f7b7c33bb17505aa7a09
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-06-26 00:17:35 +0400

    add test

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,16 @@
+// which service it is
+describe("customerservice", () => {
+  // which function it is
+
+  describe("SignIn", () => {
+    // which scenario we are testing
+
+    test("validate user input", () => {
+
+    });
+
+    test("validate reponse", () => {
+        
+    });
+  });
+});
